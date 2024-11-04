7eba657c699360d3fd6db23d451511de671fa385
pablo-rodr-bio2 <pablo.rodriguez@toptal.com>
2024-11-04 17:07:54 +0100

    Fixed test and added one to AllMeetupsPage

--- src/components/meetups/MeetupItem.test.js
+++ src/components/meetups/MeetupItem.test.js
@@ -3,6 +3,14 @@ import { shallow } from "enzyme";
 import MeetupItem from "./MeetupItem";
 
 test("<MeetupItem/> renders without crashing", () => {
-  const wrapper = shallow(<MeetupItem />);
+  const mockItem = {
+    id: "m1",
+    title: "This is a first meetup",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description: "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!"
+  }
+
+  const wrapper = shallow(<MeetupItem item={mockItem} />);
   expect(wrapper.exists()).toBe(true);
 });
---
+++ src/pages/AllMeetupsPage.test.js
@@ -0,0 +1,38 @@
+import { useFetch } from "../util-hooks/useFetch";
+import AllMeetupsPage from "./AllMeetupsPage";
+import MeetupItem from "../components/meetups/MeetupItem";
+import { shallow } from "enzyme";
+
+jest.mock("../util-hooks/useFetch")
+
+const mockItem = {
+  id: "m1",
+  title: "This is a first meetup",
+  image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+  address: "Meetupstreet 5, 12345 Meetup City",
+  description: "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!"
+}
+
+describe("AllMeetupsPage", () => {
+  describe("when the data is not loaded", () => {
+    test('it renders the loading message', () => {
+      useFetch.mockReturnValue({ data: null })
+
+      const wrapper = shallow(<AllMeetupsPage />);
+      expect(wrapper.text()).toBe('Loading...')
+    })
+  })
+
+  describe("when the data is loaded", () => {
+    test('it renders the list of meetups', () => {
+      useFetch.mockReturnValue({
+        data: [
+          mockItem
+        ]
+      })
+
+      const wrapper = shallow(<AllMeetupsPage />);
+      expect(wrapper.find(MeetupItem).length).toBe(1)
+    })
+  })
+})
